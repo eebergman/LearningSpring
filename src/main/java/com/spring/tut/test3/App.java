1bc0ba232d77e7b5e9504b3abda00d9428e0f0d1
Erin Bergman <erin.e.bergman@gmail.com>
2016-06-14 15:57:54 -0400

    ref elements

---
+++ src/main/java/com/spring/tut/test3/App.java
@@ -0,0 +1,18 @@
+package com.spring.tut.test3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/tut/beans/beans3.xml");
+		
+		Jungle jungle = (Jungle)context.getBean("jungle");
+		
+		System.out.println(jungle);
+		
+	}
+
+}
